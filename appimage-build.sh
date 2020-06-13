10b97008e17886d37beb6ffac421496a83f450f0
Travis Nickles <ryochan7@localhost.localdomain>
2020-06-13 06:01:55 -0500

    Finally got a working AppImage running
    Updated dependencies. Changed an export path

--- appimage-build.sh
+++ appimage-build.sh
@@ -48,14 +48,15 @@ set -ex		# display commands, terminate after 1st failure
 # Download deps
 download_dep "python-evdev-0.7.0" "https://github.com/gvalkov/python-evdev/archive/v0.7.0.tar.gz"
 download_dep "pylibacl-0.5.3" "https://github.com/iustin/pylibacl/releases/download/pylibacl-v0.5.3/pylibacl-0.5.3.tar.gz"
-download_dep "python-gobject-3.26.1" "https://archive.archlinux.org/packages/p/python-gobject/python-gobject-3.26.1-1-x86_64.pkg.tar.xz"
+download_dep "python-gobject-3.36.1" "https://archive.archlinux.org/packages/p/python-gobject/python-gobject-3.36.1-1-x86_64.pkg.tar.zst"
 download_dep "libpng-1.6.34" "https://archive.archlinux.org/packages/l/libpng/libpng-1.6.34-2-x86_64.pkg.tar.xz"
 download_dep "gdk-pixbuf-2.36.9" "https://archive.archlinux.org/packages/g/gdk-pixbuf2/gdk-pixbuf2-2.36.9-1-x86_64.pkg.tar.xz"
 download_dep "libcroco-0.6.13" "https://archive.archlinux.org/packages/l/libcroco/libcroco-0.6.13-1-x86_64.pkg.tar.xz"
-download_dep "libxml2-2.9.7" "https://archive.archlinux.org/packages/l/libxml2/libxml2-2.9.7%2B4%2Bg72182550-2-x86_64.pkg.tar.xz"
-download_dep "librsvg-2.42.2" "https://archive.archlinux.org/packages/l/librsvg/librsvg-2%3A2.42.2-1-x86_64.pkg.tar.xz"
-download_dep "icu-60.2" "https://archive.archlinux.org/packages/i/icu/icu-60.2-1-x86_64.pkg.tar.xz"
+download_dep "libxml2-2.9.10" "https://archive.archlinux.org/packages/l/libxml2/libxml2-2.9.10-2-x86_64.pkg.tar.zst"
+download_dep "librsvg-2.48.7" "https://archive.archlinux.org/packages/l/librsvg/librsvg-2%3A2.48.7-1-x86_64.pkg.tar.zst"
+download_dep "icu-67.1" "https://archive.archlinux.org/packages/i/icu/icu-67.1-1-x86_64.pkg.tar.zst"
 download_dep "zlib-1:1.2.11" "https://archive.archlinux.org/packages/z/zlib/zlib-1%3A1.2.11-1-x86_64.pkg.tar.xz"
+download_dep "libffi-3.3" "https://archive.archlinux.org/packages/l/libffi/libffi-3.3-3-x86_64.pkg.tar.zst"
 
 # Prepare & build deps
 export PYTHONPATH=${BUILD_APPDIR}/usr/lib/python3.8/site-packages/
@@ -70,13 +71,14 @@ fi
 build_dep "python-evdev-0.7.0"
 build_dep "pylibacl-0.5.3"
 unpack_dep "libpng-1.6.34"
-unpack_dep "python-gobject-3.26.1"
+unpack_dep "python-gobject-3.36.1"
 unpack_dep "gdk-pixbuf-2.36.9"
 unpack_dep "libcroco-0.6.13"
-unpack_dep "libxml2-2.9.7"
-unpack_dep "librsvg-2.42.2"
-unpack_dep "icu-60.2"
+unpack_dep "libxml2-2.9.10"
+unpack_dep "librsvg-2.48.7"
+unpack_dep "icu-67.1"
 unpack_dep "zlib-1:1.2.11"
+unpack_dep "libffi-3.3"
 
 # Remove uneeded files
 rm -f "${BUILD_APPDIR}/usr/${LIB}/gdk-pixbuf-2.0/2.10.0/loaders/libpixbufloader-ani.so"
--- scripts/appimage-AppRun.sh
+++ scripts/appimage-AppRun.sh
@@ -2,7 +2,7 @@
 export PATH=${APPDIR}:${APPDIR}/usr/bin:$PATH
 export LD_LIBRARY_PATH=${APPDIR}/usr/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=${APPDIR}/usr/lib64:$LD_LIBRARY_PATH
-export GI_TYPELIB_PATH=${APPDIR}/usr/lib/girepository-1.0:/usr/lib/girepository-1.0
+export GI_TYPELIB_PATH=${APPDIR}/usr/lib/girepository-1.0
 export GDK_PIXBUF_MODULEDIR=${APPDIR}/usr/lib/gdk-pixbuf-2.0/2.10.0/loaders
 export PYTHONPATH=${APPDIR}/usr/lib/python3.8/site-packages:$PYTHONPATH
 export PYTHONPATH=${APPDIR}/usr/lib64/python3.8/site-packages:$PYTHONPATH
