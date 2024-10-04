e3e25d22ae786ea02ed8b98307f65214870157a9
Martin Rys <martin@rys.rs>
2024-10-04 15:46:01 +0200

    userdata_manager: Add types and create menuicons folder - Fixes #41

--- scc/gui/userdata_manager.py
+++ scc/gui/userdata_manager.py
@@ -25,18 +25,23 @@ log = logging.getLogger("UDataManager")
 
 class UserDataManager(object):
 
-	def __init__(self):
+	def __init__(self) -> None:
 		profiles_path = get_profiles_path()
 		if not os.path.exists(profiles_path):
 			log.info("Creating profile directory '%s'", profiles_path)
 			os.makedirs(profiles_path)
+
 		menus_path = get_menus_path()
 		if not os.path.exists(menus_path):
 			log.info("Creating menu directory '%s'", menus_path)
 			os.makedirs(menus_path)
 
+		menuicons_path = get_menuicons_path()
+		if not os.path.exists(menuicons_path):
+			log.info("Creating menuicons directory '%s'", menuicons_path)
+			os.makedirs(menuicons_path)
 
-	def load_profile(self, giofile: Gio.File):
+	def load_profile(self, giofile: Gio.File) -> None:
 		"""Load profile from 'giofile' into 'profile' object.
 
 		Calls on_profiles_loaded when done
@@ -47,7 +52,7 @@ class UserDataManager(object):
 		self.on_profile_loaded(profile, giofile)
 
 
-	def save_profile(self, giofile: Gio.File, profile: Profile):
+	def save_profile(self, giofile: Gio.File, profile: Profile) -> None:
 		"""Save profile from 'profile' object into 'giofile'.
 
 		Calls on_profile_saved when done
@@ -64,29 +69,29 @@ class UserDataManager(object):
 		self.on_profile_saved(giofile)
 
 
-	def _save_profile_local(self, giofile: Gio.File, profile: Profile):
+	def _save_profile_local(self, giofile: Gio.File, profile: Profile) -> None:
 		filename = os.path.split(giofile.get_path())[-1]
 		localpath = os.path.join(get_profiles_path(), filename)
 		giofile = Gio.File.new_for_path(localpath)
 		self.save_profile(giofile, profile)
 
 
-	def load_profile_list(self, category=None):
+	def load_profile_list(self, category=None) -> None:
 		paths = [ get_default_profiles_path(), get_profiles_path() ]
 		self.load_user_data(paths, "*.sccprofile", category, self.on_profiles_loaded)
 
 
-	def load_menu_list(self, category=None):
+	def load_menu_list(self, category=None) -> None:
 		paths = [ get_default_menus_path(), get_menus_path() ]
 		self.load_user_data(paths, "*.menu", category, self.on_menus_loaded)
 
 
-	def load_menu_icons(self, category=None):
+	def load_menu_icons(self, category=None) -> None:
 		paths = [ get_default_menuicons_path(), get_menuicons_path() ]
 		self.load_user_data(paths, "*.png", category, self.on_menuicons_loaded)
 
 
-	def load_user_data(self, paths, pattern, category, callback):
+	def load_user_data(self, paths, pattern, category, callback) -> None:
 		"""Load data such as of profiles. Uses GLib to do it in the background."""
 		if category:
 			paths = [ os.path.join(p, category) for p in paths ]
@@ -105,7 +110,7 @@ class UserDataManager(object):
 			)
 
 
-	def _on_user_data_loaded(self, pdir, res, data, i, callback):
+	def _on_user_data_loaded(self, pdir, res, data, i, callback) -> None:
 		"""Called when enumerate_children_async gets lists of files.
 
 		Usually called twice for default (system) and user directory.
@@ -116,7 +121,7 @@ class UserDataManager(object):
 			# Usually when directory doesn't exists
 			log.warning("enumerate_children_finish for %s failed: %s",  pdir.get_path(), e)
 			data[i] = None, []
-		if not None in data:
+		if None not in data:
 			files = {}
 			try:
 				for pdir, enumerator in data:
@@ -139,7 +144,7 @@ class UserDataManager(object):
 			callback(files.values())
 
 
-	def _sync_load(self, pdirs):
+	def _sync_load(self, pdirs) -> dict:
 		"""Synchronous (= UI lagging) fallback method for those (hopefully) rare cases when enumerate_children_finish returns nonsense."""
 		files = {}
 		for pdir in pdirs:
@@ -148,21 +153,21 @@ class UserDataManager(object):
 		return files
 
 
-	def on_menus_loaded(self, menus): # Overriden by subclass
+	def on_menus_loaded(self, menus) -> None: # Overriden by subclass
 		pass
 
 
-	def on_profiles_loaded(self, profiles): # Overriden by subclass
+	def on_profiles_loaded(self, profiles) -> None: # Overriden by subclass
 		pass
 
 
-	def on_menuicons_loaded(self, icons): # Overriden by subclass
+	def on_menuicons_loaded(self, icons) -> None: # Overriden by subclass
 		pass
 
 
-	def on_profile_saved(self, giofile: Gio.File): # Overriden in App
+	def on_profile_saved(self, giofile: Gio.File) -> None: # Overriden in App
 		pass
 
 
-	def on_profile_loaded(self, profile: Profile, giofile: Gio.File): # Overriden in App
+	def on_profile_loaded(self, profile: Profile, giofile: Gio.File) -> None: # Overriden in App
 		pass
